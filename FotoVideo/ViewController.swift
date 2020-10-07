d8f3bad27d51d7337e3a4b5d0e409ee030c5bbfd
Gabriela Andocilla <gabriela.andocilla@udla.edu.ec>
2020-10-06 23:49:05 -0500

    Initial Commit

---
+++ FotoVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FotoVideo
+//
+//  Created by GABRIELA on 6/10/20.
+//  Copyright © 2020 com.GABRIELA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
